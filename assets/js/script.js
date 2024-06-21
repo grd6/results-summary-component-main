1aeb92cfd4d67731b7e54a35600c9defaaae99d4
grd6 <grd6@outlook.com>
2024-06-20 21:33:35 -0400

    html and js are improved

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,26 @@ const showData = async () => {
 };
 
 const updateHtml = (result) => {
+  const leftContainer = document.querySelector(".circle");
   const container = document.querySelector(".resuts-content");
- // container.innerHTML = "";
+  container.innerHTML = "";
   i = 0;
+  let totalScore = 0;
   result.forEach((item) => {
     ++i;
+    totalScore += item.score / 4;
+
     const categoryDiv = document.createElement("div");
     categoryDiv.className = `results-test-title-${i}`;
     categoryDiv.textContent = item.category;
 
-    const iconImg = document.createElement('img');
-    iconImg.className = 'icon';
+    const iconImg = document.createElement("img");
+    iconImg.className = "icon";
     iconImg.src = item.icon;
 
-
     const results1Div = document.createElement("div");
-    results1Div.className = `results-${i}`;
-   
+    results1Div.className = `results`;
+
     const resultsDiv = document.createElement("div");
     resultsDiv.className = "results";
     resultsDiv.textContent = item.score;
@@ -35,14 +38,22 @@ const updateHtml = (result) => {
     percentageDiv.className = "percentage";
     percentageDiv.textContent = "/ 100";
 
-
-    resultsDiv.appendChild(iconImg)
+    resultsDiv.appendChild(iconImg);
     resultsDiv.appendChild(percentageDiv);
     results1Div.appendChild(resultsDiv);
     categoryDiv.appendChild(results1Div);
-    
+
     container.appendChild(categoryDiv);
+
+    
   });
+  const resultdiv = document.createElement("h1");
+  resultdiv.textContent = ` ${totalScore}`;
+  const resultto = document.createElement("spam")
+  resultto.textContent="to 100"
+  leftContainer.appendChild(resultdiv);
+  leftContainer.appendChild(resultto)
+
 };
 
 showData().then((result) => {
